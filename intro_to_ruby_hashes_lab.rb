58517d1e121802ba33de21bdcdc38d33a18693b3
Bailey Wood <woodbmae@gmail.com>
2019-09-22 18:13:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,20 +11,24 @@ end
 
 def pioneer
   pioneer = {
-    :name => 
+    :name => "Grace Hopper"
   } 
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    :id => 4
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+
 end
 
 def update_counting_hash(hash, key)
